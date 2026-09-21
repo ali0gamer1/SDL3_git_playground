82a7dc1994d7b736d9b397da80407e6bb3dc1bb7
Ali <gta7iv7@gmail.com>
2026-09-21 09:41:50 +0330

    init

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include "Window.h"
+
+int main(int, char**) {
+  SDL_Init(SDL_INIT_VIDEO);
+  Window GameWindow;
+
+  bool IsRunning = true;
+  SDL_Event Event;
+  while (IsRunning) {
+    while (SDL_PollEvent(&Event)) {
+      if (Event.type == SDL_EVENT_QUIT) {
+        IsRunning = false;
+      }
+    }
+
+    GameWindow.Render();
+    GameWindow.Update();
+  }
+
+  SDL_Quit();
+  return 0;
+}
